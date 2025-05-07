59e2929c9c8f04f80ae66356f6d94cd59f560a52
feralCode0x <feralcode0x@gmail.com>
2025-05-07 16:22:04 -0400

    frontend only

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import "./App.css";
+
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Shop  from "./Pages/Shop";
+import Contact from "./Pages/Contact";
+import Blog from "./Pages/Blog";
+import Header from "./Components/Header/Navbar";
+import Footer from "./Components/Footer/Footer";
+import ProductDetails from "./Pages/ProductDetails";
+import NotFound from "./Pages/NotFound";
+import ScrollToTop from "./Components/ScrollButton/ScrollToTop";
+import Authentication from "./Pages/Authentication";
+import ResetPass from "./Components/Authentication/Reset/ResetPass";
+import BlogDetails from "./Components/Blog/BlogDetails/BlogDetails";
+import TermsConditions from "./Pages/TermsConditions";
+import ShoppingCart from "./Components/ShoppingCart/ShoppingCart";
+import Popup from "./Components/PopupBanner/Popup";
+import Toaster from "react-hot-toast";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Popup />
+      <ScrollToTop />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Shop" element={<Shop />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/product" element={<ProductDetails />} />
+          <Route path="/loginSignUp" element={<Authentication />} />
+          <Route path="/resetPassword" element={<ResetPass />} />
+          <Route path="/BlogDetails" element={<BlogDetails />} />
+          <Route path="/terms" element={<TermsConditions />} />
+          <Route path="/cart" element={<ShoppingCart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+        <Toaster />
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
